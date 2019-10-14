72d5275f54792155fd3a54c07b1e2d293796db88
admers cardenas <a.cardunajma@gmail.com>
2019-10-14 00:16:49 -0500

    v4

--- ajax/buscarRS.php
+++ ajax/buscarRS.php
@@ -5,6 +5,7 @@ require_once "../core/configSite.php";
 require_once "../controllers/adminController.php";
 
 $val=$_POST['valor'];
+
 if(!empty($val))
 {
     $valor=$_POST['valor'];
@@ -13,3 +14,22 @@ if(!empty($val))
     //print_r($r);
     echo json_encode($r);
 }
+
+
+
+if (isset($_POST['idV'])){
+    $insertv=$_POST['insertV'];
+    $idv=$_POST['idV'];
+    $db_handle->updGConsumo($insertv,$idv);
+    /*
+        $queryUpd = "UPDATE factura_recibo SET consumo = $editv WHERE  idfactura_recibo= $idv";
+        $result = $db_handle->consultaAsociado($queryUpd);
+    */
+
+    echo $result;
+}
+
+
+
+
+
